d11bb12c04a13a6d01161ec4e8edb9917d233968
Dustin Thomas <stdio@cptlobster.dev>
2024-04-04 23:33:26 -0500

    stats

--- download_wp_dump.py
+++ download_wp_dump.py
@@ -8,6 +8,8 @@ import json
 from urllib import request
 from bz2 import BZ2Decompressor
 from rich.progress import Progress
+from timeit import default_timer as timer
+from datetime import timedelta
 
 # Mirror URL. Make sure that the mirror supports HTTPS.
 WPDUMP_MIRROR = "dumps.wikimedia.org"
@@ -21,6 +23,14 @@ TARGET_PATH = "wp-data"
 WPDUMP_SOURCE = "articlesmultistreamdump"
 # Default chunk size (bytes)
 CHUNK_SIZE = 100 * 1024
+
+
+def sizeof_fmt(num, suffix="B"):
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
 
 
 def get_wd_url(path: str = "",
@@ -60,7 +70,7 @@ def download_and_unzip(path: str,
                        mirror: str = None,
                        wiki: str = None,
                        date: str = None,
-                       progress: Progress = None) -> bool:
+                       progress: Progress = None) -> (int, int):
     """Download and unzip a file automatically.
 
     :param path: The relative path of the file to download
@@ -71,13 +81,16 @@ def download_and_unzip(path: str,
     url = get_wd_url(path, mirror, wiki, date)
     dc = BZ2Decompressor()
     leftover = b""
+    dc_length = 0
     with req.get(url, stream=True) as response:
         length = int(response.headers['Content-Length'])
         if progress:
-            current_dl = progress.add_task(f"Downloading {path}", total=length)
+            current_dl = progress.add_task(f"Downloading {path} ({sizeof_fmt(length)})", total=length)
         with open(f"{TARGET_PATH}/{path.removesuffix('.bz2')}", "wb") as f:
             for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-                f.write(dc.decompress(leftover + chunk))
+                decompressed = dc.decompress(leftover + chunk)
+                f.write(decompressed)
+                dc_length += len(decompressed)
                 leftover = b""
                 if progress:
                     progress.update(current_dl, advance=CHUNK_SIZE)
@@ -86,7 +99,7 @@ def download_and_unzip(path: str,
                     # you should see that 'leftover' is the start of a new stream
                     # we have to start a new decompressor
                     dc = BZ2Decompressor()
-    return True
+    return (length, dc_length)
 
 
 def get_files_in_dump(metadata: dict, dump: str = None) -> list:
@@ -109,11 +122,26 @@ def download_dump(dump: str = None,
         success = success and download_and_unzip(path, mirror, wiki, date)
     return success
 
+
 if __name__ == "__main__":
     with Progress() as progress:
         md = get_metadata()
         files = get_files_in_dump(md)
         overall_task = progress.add_task("Downloading xmldumps", total=len(files))
+        overall_bytes = 0
+        overall_dc_bytes = 0
+        start = timer()
         for path in files:
-            download_and_unzip(path, progress=progress)
+            nb, ndb = download_and_unzip(path, progress=progress)
+            overall_bytes += nb
+            overall_dc_bytes += ndb
             progress.update(overall_task, advance=1)
+        end = timer()
+        progress.console.print(f"Download completed.\n\n"
+                               f"DOWNLOAD STATISTICS\n"
+                               f"Size (compressed): {sizeof_fmt(overall_bytes)}\n"
+                               f"Size (decompressed): {sizeof_fmt(overall_dc_bytes)}"
+                               f"Average file size (compressed): {sizeof_fmt(overall_bytes / len(files))}\n"
+                               f"Average file size (decompressed): {sizeof_fmt(overall_dc_bytes / len(files))}\n"
+                               f"Total time elapsed: {str(timedelta(seconds=end - start))}\n"
+                               f"Average time per file: {str(timedelta(seconds=(end - start) / len(files)))}")
