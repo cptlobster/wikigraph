1d1aff66c45ef19abe1fc3f0698543f92abfc5dd
Dustin Thomas <stdio@cptlobster.dev>
2024-12-19 19:08:27 -0500

    create stub of XML parser

---
+++ src/main/scala/WMXMLDumpParser.scala
@@ -0,0 +1,8 @@
+case class WMXMLDumpParser():
+  /**
+   * Split an XML dump by page
+   * @param xml Full (or partial) XML dump as a string
+   * @return List of strings
+   */
+  def split_by_page(xml: String): List[String] =
+    ???
