3ca001acd087f0b91d9143aa03ff411d15b1f02c
Dustin Thomas <stdio@cptlobster.dev>
2024-12-19 19:08:18 -0500

    better documentation

--- src/main/scala/WikitextParser.scala
+++ src/main/scala/WikitextParser.scala
@@ -1,6 +1,11 @@
 import scala.annotation.tailrec
 
-class WikitextParser:
+case class WikitextParser():
+  /**
+   * Get all the links present in a single WikiText page.
+   * @param contents WikiText formatted page
+   * @return A list of page titles linked to by this page
+   */
   def read_page(contents: String): List[String] =
     @tailrec def rec_read_page(contents: String, links: List[String]): List[String] =
       if contents.isEmpty then links else
@@ -11,19 +16,36 @@ class WikitextParser:
 
     rec_read_page(contents, Nil)
 
+  /**
+   * Drop all contents before and including the beginning of a link (double square bracket)
+   * @param contents String to extract from
+   * @return All contents to the right of the beginning of the link
+   */
   @tailrec private def until_brackets(contents: String): String =
     if contents.isEmpty then "" else
       contents.take(2) match
         case "[[" => contents.drop(2)
         case _ => until_brackets(contents.tail)
 
+  /**
+   * Extract the first possible link from the source string (This requires being called at the beginning of a link to
+   * work properly!)
+   * @param contents String to extract from
+   * @return A tuple containing the link text and the rest of the content. If it fails to reach the end of the link, the
+   *         right side will be empty, if it fails to get the linked page the left side will be empty.
+   */
   private def dissect_link(contents: String): (String, String) =
-    val con_split = split_first(contents, "]]")
-    val rawlink = con_split._1
-    val rest = con_split._2
-    val link = split_first(rawlink, "|")._1
+    val (rawlink, rest) = split_first(contents, "]]")
+    val (link, _) = split_first(rawlink, "|")
     (link, rest)
 
+  /**
+   * Split a string at the first instance of an exactly matching delimiter
+   * @param contents String to split
+   * @param criteria The exact match of what string to split on
+   * @return A tuple containing the left and right sides of the split. If the split fails, the right side will be an
+   *         empty string.
+   */
   private def split_first(contents: String, criteria: String): (String, String) =
     @tailrec def sp(con: String, acc: String): (String, String) =
       if con.isEmpty || con.startsWith(criteria) then (acc, con) else sp(con.tail, acc :+ con.head)
--- src/test/scala/WikitextParserSuite.scala
+++ src/test/scala/WikitextParserSuite.scala
@@ -5,13 +5,16 @@ class WikitextParserSuite extends munit.FunSuite {
   val wp: WikitextParser = WikitextParser()
   // Sample 1 from "Everything Relative" (https://en.wikipedia.org/wiki/Everything_Relative)
   val sample1 = "'''''Everything Relative''''' is a 1996 American [[comedy-drama]] [[independent film]] written and directed by [[Sharon Pollack (filmmaker)|Sharon Pollack]]. It centers around a weekend reunion of seven women who were friends and political activists in college. The film has been compared to [[The Big Chill (film)|The Big Chill]] and [[Return of the Secaucus Seven]] in terms of theme and structure.<ref>{{cite web |url= http://www.sfgate.com/movies/article/FILM-REVIEW-Affection-Rescues-Relative-2859749.php |title=Affection Rescues 'Relative' |last=Guthmann|first=Edward|work=[[San Francisco Chronicle]]|date=January 10, 1997|access-date=2016-05-24}}</ref> It was presented at the 1996 [[Sundance Film Festival]] as part of the American Spectrum lineup.<ref name=\"Sundance\">{{cite web |url=http://www.filmscouts.com/scripts/festarchive.cfm?name=sundan96/films |title=Film Scouts: 1996 Sundance Film Festival Program |access-date= 2016-05-24}}</ref>"
+  // TODO: add more samples
+
+  // ensure that the WikiText parser works
   test("Can get linktext from a WikiText document") {
     val obtained = wp.read_page(sample1)
-    val expected = "Sundance Film Festival" :: "San Francisco Chronicle" :: "Return of the Secaucus Seven" :: 
+    // expected link texts
+    val expected = "Sundance Film Festival" :: "San Francisco Chronicle" :: "Return of the Secaucus Seven" ::
       "The Big Chill (film)" :: "Sharon Pollack (filmmaker)" :: "independent film" :: "comedy-drama" :: Nil
-    println(obtained.mkString("[\"", "\",\"", "\"]"))
     for (link <- obtained) {
-      assert(expected.contains(link))
+      assert(expected.contains(clue(link)))
     }
   }
 }
