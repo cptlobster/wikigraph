492461e03874e25a073a1f8a9fdd8794f71e7f79
Dustin Thomas <stdio@cptlobster.dev>
2024-12-28 11:57:41 -0500

    setup an XML parser

---
+++ src/main/scala/dev/cptlobster/wikigraph/Page.scala
@@ -0,0 +1,3 @@
+package dev.cptlobster.wikigraph
+
+case class Page(title: String, id: Int, namespace: Int, contents: String)
--- src/main/scala/dev/cptlobster/wikigraph/WMXMLDumpParser.scala
+++ src/main/scala/dev/cptlobster/wikigraph/WMXMLDumpParser.scala
@@ -1,8 +1,28 @@
 package dev.cptlobster.wikigraph
 
-import dev.cptlobster.wikigraph.PartitionedFileInputStream
+import dev.cptlobster.wikigraph.{Page, PartitionedFileInputStream}
 
-import scala.xml.XML
+import java.io.InputStream
+import java.util
+import scala.xml.{Node, NodeSeq, XML}
+import scala.jdk.CollectionConverters._
 
 case class WMXMLDumpParser():
-  def openPartitions(partitions: List[String]): PartitionedFileInputStream = ???
+  def openPartitions(partitions: List[String]): PartitionedFileInputStream =
+    val pl: util.ArrayList[String] = new util.ArrayList[String](partitions.asJava)
+    new PartitionedFileInputStream(partitions)
+
+  def get_xml(stream: InputStream): NodeSeq =
+    val xml = XML.load(stream)
+    xml \\ "mediawiki"
+
+  def get_namespaces(nodes: NodeSeq): Map[Int, String] =
+    (nodes \\ "namespace").map((node: Node) => ((node \@ "key").toInt, node.text)).toMap[Int, String]
+
+  def get_pages(nodes: NodeSeq): Unit =
+    for (node <- nodes if node.label == "page") yield
+      Page(
+        (node \\ "title").text,
+        (node \\ "id").text.toInt,
+        (node \\ "ns").text.toInt,
+        ((node \\ "revision").head \\ "text").text)
