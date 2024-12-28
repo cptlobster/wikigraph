24ea73bb3a462b8b7d78e8edce03a498c3d135c5
Dustin Thomas <stdio@cptlobster.dev>
2024-12-28 16:24:57 -0500

    change xpath projection to not check subsequences

--- src/main/scala/dev/cptlobster/wikigraph/WMXMLDumpParser.scala
+++ src/main/scala/dev/cptlobster/wikigraph/WMXMLDumpParser.scala
@@ -9,17 +9,17 @@ case class RawPage(title: String, id: Int, namespace: Int, contents: String)
 case class WMXMLDumpParser():
   def get_xml(stream: InputStream): NodeSeq =
     val xml = XML.load(stream)
-    xml \\ "mediawiki"
+    xml \ "mediawiki"
 
   def get_namespaces(nodes: NodeSeq): Map[Int, String] =
-    (nodes \\ "namespace").map((node: Node) => ((node \@ "key").toInt, node.text)).toMap[Int, String]
+    (nodes \ "namespace").map((node: Node) => ((node \@ "key").toInt, node.text)).toMap[Int, String]
 
   def get_pages(nodes: NodeSeq): Seq[RawPage] =
     for (node <- nodes if node.label == "page") yield parse_page(node)
 
   def parse_page(node: Node): RawPage =
     RawPage(
-      (node \\ "title").head.text,
-      (node \\ "id").head.text.toInt,
-      (node \\ "ns").head.text.toInt,
-      ((node \\ "revision").head \\ "text").head.text)
+      (node \ "title").head.text,
+      (node \ "id").head.text.toInt,
+      (node \ "ns").head.text.toInt,
+      ((node \ "revision").head \ "text").head.text)
